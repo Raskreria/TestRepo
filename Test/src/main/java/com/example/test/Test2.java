1f865dc1e1832e28f0b2130c63f85823cc8c5909
NORTH205-19\북205_20 <knkn9436@naver.com>
2023-10-25 11:53:37 +0900

    브랜치 작업했어요

--- Test/src/main/java/com/example/test/Test2.java
+++ Test/src/main/java/com/example/test/Test2.java
@@ -8,5 +8,6 @@ public class Test2 {
         System.out.println("Test2에서 브랜치 추가 작업 또!");
         System.out.println("Test2 브랜치에서 추가작업 발생!");
         System.out.println("Test2 브랜치에서 추가작업 발생!");
+        System.out.println("Test2 브랜치에서 추가작업 발생!");
     }
 }
