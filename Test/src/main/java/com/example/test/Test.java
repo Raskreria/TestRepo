76001dd00bf38a573b03248f0528db3638d166a5
NORTH205-19\북205_20 <knkn9436@naver.com>
2023-10-25 11:49:23 +0900

    메인에서 작업

--- Test/src/main/java/com/example/test/Test.java
+++ Test/src/main/java/com/example/test/Test.java
@@ -9,6 +9,6 @@ public class Test {
 
         System.out.println("메인에서 추가 sout 작성 또 다시");
         System.out.println("메인에서 추가 sout 작성 또 다시");
-
+        System.out.println("메인에서 추가 sout 작성 또 다시");
     }
 }
