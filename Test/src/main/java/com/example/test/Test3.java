2b3edb93bd0a0305bc0f62c8e4f16f8de2548ff3
NORTH205-19\북205_20 <knkn9436@naver.com>
2023-10-25 12:00:32 +0900

    Test3 브랜치에서 작업했어요

--- Test/src/main/java/com/example/test/Test3.java
+++ Test/src/main/java/com/example/test/Test3.java
@@ -3,5 +3,6 @@ package com.example.test;
 public class Test3 {
     public static void main(String[] args) {
         System.out.println("Test3브랜치에서 sout");
+        System.out.println("Test3브랜치에서 sout");
     }
 }
