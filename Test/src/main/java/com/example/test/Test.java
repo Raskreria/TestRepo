329cc76a6d036e4e47cfc8d94faf53269f38da67
NORTH205-19\북205_20 <knkn9436@naver.com>
2023-10-25 11:40:41 +0900

    메인 수정

--- Test/src/main/java/com/example/test/Test.java
+++ Test/src/main/java/com/example/test/Test.java
@@ -3,11 +3,8 @@ package com.example.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("메인에서 sout작성");
-<<<<<<< HEAD
         System.out.println("메인에서 추가 sout 작성");
         System.out.println("메인에서 추가 sout 작성 또");
-=======
-        System.out.println("메인에서 추가 sout 작성");
->>>>>>> feature/Test2
+
     }
 }
