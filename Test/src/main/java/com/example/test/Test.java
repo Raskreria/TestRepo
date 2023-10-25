13bfd7f6658df57338b75142bf55d55a0afa8fd7
NORTH205-19\북205_20 <knkn9436@naver.com>
2023-10-25 11:36:19 +0900

    메인에서 추가작업 또

--- Test/src/main/java/com/example/test/Test.java
+++ Test/src/main/java/com/example/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("메인에서 sout작성");
         System.out.println("메인에서 추가 sout 작성");
+        System.out.println("메인에서 추가 sout 작성 또");
     }
 }
