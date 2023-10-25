49455e1503797d00df7b517b3cae42e2f7d6c1ac
NORTH205-19\북205_20 <knkn9436@naver.com>
2023-10-25 11:42:45 +0900

    브랜치에서 작업 중 메인 실수로 수정함

--- Test/src/main/java/com/example/test/Test.java
+++ Test/src/main/java/com/example/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("메인에서 sout작성");
         System.out.println("메인에서 추가 sout 작성");
-        System.out.println("메인에서 추가 sout 작성 또");
+        System.out.println("메인에서 추가 sout 작성 ");
 
     }
 }
