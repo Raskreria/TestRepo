706f19f53678ad477b4afae92fb7879d26f7c3e7
NORTH205-19\북205_20 <knkn9436@naver.com>
2023-10-25 11:20:18 +0900

    메인에서 추가 작업이 갑자기 발생함

--- Test/src/main/java/com/example/test/Test.java
+++ Test/src/main/java/com/example/test/Test.java
@@ -3,5 +3,6 @@ package com.example.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("메인에서 sout작성");
+        System.out.println("메인에서 추가 sout 작성");
     }
 }
